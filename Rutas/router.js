b701684ac42e680c36a5b1bfe825d1b1f29a17c4
Madero26 <jose.madero244903@potros.itson.edu.mx>
2024-11-10 19:10:08 -0700

    nuevo diseño formularios
    faltaron algunos formularios de hacer y aun no se mete logica

--- Rutas/router.js
+++ Rutas/router.js
@@ -7,6 +7,7 @@ router.get('/', (req, res)=> {
     res.render('index')
 })
 
+//RUTAS DE PAGINA PRINCIPAL
 router.get('/login', (req, res)=>{
     res.render('Login')
 })
@@ -25,6 +26,128 @@ router.get('/Coord', (req, res)=>{
 router.get('/Standing', (req, res)=>{
     res.render('Standing')
 })
+
+
+
+//RUTAS DE NAVEGADOR DE ESTADISTICAS ADMINISTRADOR
+router.get('/formEstadisticas', authControlador.isAuthenticated, (req, res)=>{
+    res.render('AdminEstadisticas/FormularioDatos_AdminEstadisticas', { usuario: req.user })
+})
+router.get('/formEstadisticaPitcheo', authControlador.isAuthenticated, (req, res)=>{
+    res.render('AdminEstadisticas/FormularioEstadisticasDePitcheo', { usuario: req.user })
+})
+router.get('/formEstadisticasBateo', authControlador.isAuthenticated, (req, res)=>{
+    res.render('AdminEstadisticas/FormularioEstadisticasDeBateo', { usuario: req.user })
+})
+router.get('/formContraNuevaEstadisticas', authControlador.isAuthenticated, (req, res)=>{
+    res.render('AdminEstadisticas/contraAdminEstadisticas', { usuario: req.user })
+})
+
+//RUTAS DE NAVEGADOR ADMINISTRADOR GENERAL
+                //FOTOS
+router.get('/formAdmin', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormFotosCategoria', { usuario: req.user })
+})
+router.get('/formFotoCoordinador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormFotoCoordinador', { usuario: req.user })
+})
+router.get('/formAFotoEntrenadores', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormFotosEntrenadores', { usuario: req.user })
+})
+router.get('/formFotoEquipo', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormsFotosEquipo', { usuario: req.user })
+})
+router.get('/formFotoJugador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormsFotosJugador', { usuario: req.user })
+})
+
+                //Actualizar
+router.get('/formActualizarCategoria', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioActualizarCategoria', { usuario: req.user })
+})
+router.get('/formActualizarCoordinador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioActualizarCoordinador', { usuario: req.user })
+})
+router.get('/formActualizarEntrenador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioActualizarEntrenador', { usuario: req.user })
+})
+router.get('/formActualizarEquipo', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioActualizarEquipo', { usuario: req.user })
+})
+router.get('/formActualizarJugador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioActualizarJugador', { usuario: req.user })
+})
+router.get('/formActualizarPago', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioActualizarPagoSemanal', { usuario: req.user })
+})
+router.get('/formActualizarUsuarioCoordinador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioActualizarUsuarioCoordinador', { usuario: req.user })
+})
+router.get('/formActualizarUsuarioJugador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioActualizarUsuarioJugador', { usuario: req.user })
+})
+                //Eliminar
+router.get('/formEliminarCategoria', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioEliminarCategoria', { usuario: req.user })
+})
+router.get('/formEliminarCoordinador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioEliminarCoordinador', { usuario: req.user })
+})
+router.get('/formEliminarEntrenador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioEliminarEntrenador', { usuario: req.user })
+})
+router.get('/formEliminarEquipo', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioEliminarEquipo', { usuario: req.user })
+})
+router.get('/formEliminarJugador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioEliminarJugador', { usuario: req.user })
+})
+router.get('/formEliminarPago', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioEliminarPagoSemanal', { usuario: req.user })
+})
+router.get('/formEliminarUsuarioCoordinador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioEliminarUsuarioCoordinador', { usuario: req.user })
+})
+router.get('/formEliminarUsuarioJugador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioEliminarUsuarioJugador', { usuario: req.user })
+})
+                //Crear o Agregar
+router.get('/formAgregarPago', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioAgregarCuotaJugador', { usuario: req.user })
+})
+router.get('/formCrearCategoria', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioCrearCategoria', { usuario: req.user })
+})
+router.get('/formCrearCoordinadores', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioCrearCoordinadores', { usuario: req.user })
+})
+router.get('/formCrearEntrenadores', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioCrearEntrenadores', { usuario: req.user })
+})
+router.get('/formCrearEquipos', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioCrearEquipos', { usuario: req.user })
+})
+router.get('/formCrearJugador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioCrearJugador', { usuario: req.user })
+})
+router.get('/formCrearUsuarioJugador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioCrearUsuarioJugador', { usuario: req.user })
+})
+router.get('/formCrearUsuarioCoordinador', authControlador.isAuthenticated,  (req, res)=>{
+    res.render('AdminGeneral/FormularioCrearUsuarioCoordinador', { usuario: req.user })
+})
+
+
+
+
+router.get('/AdminContra', authControlador.isAuthenticated, (req, res) => {
+    res.render('AdminGeneral/contraNuevaAdmin', { usuario: req.user})
+})
+
+//RUTAS DE NAVEGADOR COORDINADORES
+router.get('/formCoordinadores', authControlador.isAuthenticated, (req, res) => {
+    res.render('Coordinador/FormFotosEquipos', { usuario: req.user})
+})
 router.get('/CoordAgregar', authControlador.isAuthenticated,(req, res)=>{
     res.render('FormularioAgregarJugador', { usuario: req.user})
 })
@@ -38,31 +161,22 @@ router.get('/CoordPagos', authControlador.isAuthenticated,(req, res)=>{
     res.render('FormsPagos', { usuario: req.user})
 })
 router.get('/CoordContraNueva', authControlador.isAuthenticated, (req, res)=>{
-    res.render('contraNuevaCoordinadores', { usuario: req.user})
+    res.render('Coordinador/contraNuevaCoordinadores', { usuario: req.user})
 })
 router.get('/CoordFotoCate', authControlador.isAuthenticated,(req, res)=>{
     res.render('FormFotosCategoriaIndividual', { usuario: req.user})
 })
-router.get('/CoordFotoCate', authControlador.isAuthenticated,(req, res)=>{
-    res.render('FormFotosCategoriaIndividual', { usuario: req.user})
-})
+
+//---------------------Formularios------------------------------
 
 
-router.get('/formEstadisticas', authControlador.isAuthenticated, (req, res)=>{
-    res.render('FormularioDatos_AdminEstadisticas', { usuario: req.user })
-})
-router.get('/formAdmin', authControlador.isAuthenticated,  (req, res)=>{
-    res.render('FormFotosCategoria', { usuario: req.user })
-})
+//Formularios Estadisticas
 
-router.get('/formCoordinadores', authControlador.isAuthenticated, (req, res) => {
-    res.render('FormFotosEquipos', { usuario: req.user})
-})
-router.get('/AdminContra', authControlador.isAuthenticated, (req, res) => {
-    res.render('contraNuevaAdmin', { usuario: req.user})
-})
-
+//Formularios Admin General
 router.post('/formAdmin', authControlador.formAdmin) // Referencia correcta
+//Formularios Coordinadores
+
+
 router.post('/login', authControlador.inicio) // Referencia correcta
 router.get('/logout', authControlador.logout)
 
