8534f621c135d7f9ab61503272881551838180eb
Madero26 <jose.madero244903@potros.itson.edu.mx>
2024-12-17 15:03:59 -0700

    actualizar listo y mitad de eliminar
    falta el back de todos los formularios finales para actualizar y terminar los filtros que me faltaron de eliminar y el back final de los fomularios

--- Rutas/router.js
+++ Rutas/router.js
@@ -194,7 +194,7 @@ router.get('/formEliminarJugador', authControlador.isAuthenticated,  (req, res)=
     }
     );
 })
-router.get('/formEliminarPago', authControlador.isAuthenticated,  (req, res)=>{
+router.get('/formEliminarPagoSemanal', authControlador.isAuthenticated,  (req, res)=>{
     coneccion.query("SELECT * FROM jugadores", function(error, rows){
         if(error){
             throw error;
@@ -282,10 +282,7 @@ router.get('/formCrearEquipos', authControlador.isAuthenticated,  (req, res)=>{
             });
              
         }
-    }
-
-
-    );
+    });
     
 })
 router.get('/formCrearJugador', authControlador.isAuthenticated,  (req, res)=>{
@@ -320,6 +317,8 @@ router.get('/AdminContra', authControlador.isAuthenticated, (req, res) => {
     res.render('AdminGeneral/contraNuevaAdmin', { usuario: req.user})
 })
 
+//ELIMINAR PERO SOLO CONFIRMAR
+
 router.get('/eliminarCategoria', authControlador.isAuthenticated, (req, res) => {
     const categoriaID = req.query.categoriaID; 
     // Consulta la base de datos para obtener los datos de la categoría
@@ -348,6 +347,20 @@ router.get('/eliminarEntrenador', authControlador.isAuthenticated, (req, res) =>
         }
     });
 });
+router.get('/eliminarEntrenadorNuevo', authControlador.isAuthenticated, (req, res) => {
+    const entrenador = req.query.entrenador; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM entrenadores WHERE id_entrenador = ?", [entrenador], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/EliminarEntrenador', { usuario: req.user, entrenadores: rows[0] });
+        }
+    });
+});
 router.get('/eliminarEquipo', authControlador.isAuthenticated, (req, res) => {
     const categoriaID = req.query.categoriaID; 
     // Consulta la base de datos para obtener los datos de la categoría
@@ -359,6 +372,20 @@ router.get('/eliminarEquipo', authControlador.isAuthenticated, (req, res) => {
         } else {
             // Renderiza la vista con los datos de la categoría
             res.render('AdminGeneral/EliminarConfirmacionEquipo', { usuario: req.user, equipos: rows });
+        }
+    });
+});
+router.get('/eliminarEquipoNuevo', authControlador.isAuthenticated, (req, res) => {
+    const equipo = req.query.equipo; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM equipos WHERE id_equipo = ?", [equipo], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/AEliminarEquipo', { usuario: req.user, equipos: rows[0] });
         }
     });
 });
@@ -376,6 +403,20 @@ router.get('/eliminarJugador', authControlador.isAuthenticated, (req, res) => {
         }
     });
 });
+router.get('/eliminarJugadorNuevo', authControlador.isAuthenticated, (req, res) => {
+    const jugador = req.query.jugador; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM jugadores WHERE id_jugador = ?", [jugador], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/EliminarJugador', { usuario: req.user, jugadores: rows[0] });
+        }
+    });
+});
 router.get('/eliminarUsuarioJugador', authControlador.isAuthenticated, (req, res) => {
     const categoriaID = req.query.categoriaID; 
     // Consulta la base de datos para obtener los datos de la categoría
@@ -387,6 +428,20 @@ router.get('/eliminarUsuarioJugador', authControlador.isAuthenticated, (req, res
         } else {
             // Renderiza la vista con los datos de la categoría
             res.render('AdminGeneral/EliminarConfirmacionUsuarioJugador', { usuario: req.user, jugadores: rows });
+        }
+    });
+});
+router.get('/eliminarUsuarioJugadorNuevo', authControlador.isAuthenticated, (req, res) => {
+    const jugador = req.query.jugador; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM usuariofamiliarpago WHERE id_jugador = ?", [jugador], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/EliminarUsuarioJugador', { usuario: req.user, usuarios: rows[0]  });
         }
     });
 });
@@ -421,14 +476,28 @@ router.get('/eliminarCategoria', authControlador.isAuthenticated, (req, res) => 
 router.get('/eliminarPagoSemanal', authControlador.isAuthenticated, (req, res) => {
     const jugador = req.query.jugador; 
     // Consulta la base de datos para obtener los datos de la categoría
-    coneccion.query("SELECT * FROM jugadores WHERE id_jugador = ?", [jugador], (error, rows) => {
+    coneccion.query("SELECT * FROM pagos WHERE id_jugador = ?", [jugador], (error, rows) => {
         if (error) {
             console.error('Error al consultar la categoría:', error);
             res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
         
         } else {
             // Renderiza la vista con los datos de la categoría
-            res.render('AdminGeneral/EliminarConfirmacionPagoSemanal', { usuario: req.user, jugadores: rows[0] });
+            res.render('AdminGeneral/EliminarConfirmacionPagoSemanal', { usuario: req.user, pagos: rows });
+        }
+    });
+});
+router.get('/eliminarPagoSemanalNuevo', authControlador.isAuthenticated, (req, res) => {
+    const semana = req.query.semana; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM pagos WHERE id = ?", [semana], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/EliminarPagoSemanal', { usuario: req.user, semana: rows[0] });
         }
     });
 });
@@ -443,6 +512,242 @@ router.get('/eliminarUsuarioCoordinador', authControlador.isAuthenticated, (req,
         } else {
             // Renderiza la vista con los datos de la categoría
             res.render('AdminGeneral/EliminarConfirmacionUsuarioCoordinador', { usuario: req.user, user: rows[0] });
+        }
+    });
+});
+
+
+//ACTUALIZAR PERO SOLO CONFIRMAR
+
+router.get('/actualizaCategoria', authControlador.isAuthenticated, (req, res) => {
+    const categoriaID = req.query.categoriaID; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM Categorias WHERE id_categoria = ?", [categoriaID], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionCategoria', { usuario: req.user, categoria: rows[0] });
+        }
+    });
+});
+router.get('/actualizaEntrenador', authControlador.isAuthenticated, (req, res) => {
+    const categoriaID = req.query.categoriaID; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM entrenadores WHERE id_categoria = ?", [categoriaID], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionEntrenador', { usuario: req.user, entrenadores: rows });
+        }
+    });
+});
+router.get('/actualizaEntrenadorNuevo', authControlador.isAuthenticated, (req, res) => {
+    const entrenador = req.query.entrenador; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM entrenadores WHERE id_entrenador = ?", [entrenador], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la 
+            coneccion.query("SELECT * FROM Categorias", function(error, rows2){
+                if(error){
+                    throw error;
+                }else{            
+                    res.render('AdminGeneral/ActualizarEntrenador', { usuario: req.user, entrenadores: rows[0], categorias: rows2 });
+
+                }
+            });
+        }
+    });
+});
+router.get('/actualizaEquipo', authControlador.isAuthenticated, (req, res) => {
+    const categoriaID = req.query.categoriaID; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM equipos WHERE id_categoria = ?", [categoriaID], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionEquipo', { usuario: req.user, equipos: rows });
+        }
+    });
+});
+router.get('/actualizaEquipoNuevo', authControlador.isAuthenticated, (req, res) => {
+    const equipo = req.query.equipo; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM equipos WHERE id_equipo = ?", [equipo], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            coneccion.query("SELECT * FROM Categorias", function(error, rows2){
+                if(error){
+                    throw error;
+                }else{
+                    coneccion.query("SELECT * FROM entrenadores", function(errores, row){
+                        if(error){
+                            throw error;
+                        }else{
+                       // Renderiza la vista con los datos de la categoría
+                            res.render('AdminGeneral/ActualizarEquipo', { usuario: req.user, equipos: rows[0], categorias: rows2, entrenadores: row });
+                        }   
+                    });
+                     
+                }
+            });
+        }
+    });
+});
+router.get('/actualizaJugador', authControlador.isAuthenticated, (req, res) => {
+    const categoriaID = req.query.categoriaID; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM jugadores WHERE id_categoria = ?", [categoriaID], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionJugador', { usuario: req.user, jugadores: rows });
+        }
+    });
+});
+router.get('/actualizaJugadorNuevo', authControlador.isAuthenticated, (req, res) => {
+    const jugador = req.query.jugador; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM jugadores WHERE id_jugador = ?", [jugador], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            const categoriaId = rows[0].id_categoria;
+
+            coneccion.query("SELECT * FROM equipos WHERE id_categoria = ?", [categoriaId], (error, rows2) => {
+                if (error) {
+                    console.error('Error al consultar la categoría:', error);
+                    res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+                
+                } else {
+                    coneccion.query("SELECT * FROM Categorias", function(error, rows3){
+                        if(error){
+                            throw error;
+                        }else{
+                           // Renderiza la vista con los datos de la categoría
+                    res.render('AdminGeneral/ActualizarJugador', { usuario: req.user, jugadores: rows[0], categorias: rows3, equipos: rows2 });
+                        }
+                    });
+                }
+            });
+           }
+    });
+});
+router.get('/actualizaUsuarioJugador', authControlador.isAuthenticated, (req, res) => {
+    const categoriaID = req.query.categoriaID; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM jugadores WHERE id_categoria = ?", [categoriaID], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionUsuarioJugador', { usuario: req.user, jugadores: rows });
+        }
+    });
+});
+router.get('/actualizaUsuarioJugadorNuevo', authControlador.isAuthenticated, (req, res) => {
+    const jugador = req.query.jugador; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM usuariofamiliarpago WHERE id_jugador = ?", [jugador], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarUsuarioJugador', { usuario: req.user, usuarios: rows[0] });
+        }
+    });
+});
+router.get('/actualizaCoordinador', authControlador.isAuthenticated, (req, res) => {
+    const coordinador = req.query.coordinador; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM Coordinadores WHERE id_coordinador = ?", [coordinador], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionCoordinador', { usuario: req.user, coordinador: rows[0] });
+        }
+    });
+});
+router.get('/actualizaCategoria', authControlador.isAuthenticated, (req, res) => {
+    const usuario = req.query.usuario; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM UsuarioAdministradores WHERE id_usuario = ?", [usuario], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionUsuarioCoordinador', { usuario: req.user, usuario: rows[0] });
+        }
+    });
+});
+router.get('/actualizaPagoSemanal', authControlador.isAuthenticated, (req, res) => {
+    const jugador = req.query.jugador; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM pagos WHERE id_jugador = ?", [jugador], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionPagoSemanal', { usuario: req.user, pagos: rows });
+        }
+    });
+});
+router.get('/actualizaPagoSemanalNuevo', authControlador.isAuthenticated, (req, res) => {
+    const semana = req.query.semana; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM pagos WHERE id = ?", [semana], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarPagoSemanal', { usuario: req.user, semana: rows[0] });
+        }
+    });
+});
+router.get('/actualizaUsuarioCoordinador', authControlador.isAuthenticated, (req, res) => {
+    const usuario = req.query.usuario; 
+    // Consulta la base de datos para obtener los datos de la categoría
+    coneccion.query("SELECT * FROM UsuarioAdministradores WHERE id_usuario = ?", [usuario], (error, rows) => {
+        if (error) {
+            console.error('Error al consultar la categoría:', error);
+            res.status(500).send('Hubo un problema al obtener los datos de la categoría.');
+        
+        } else {
+            // Renderiza la vista con los datos de la categoría
+            res.render('AdminGeneral/ActualizarConfirmacionUsuarioCoordinador', { usuario: req.user, user: rows[0] });
         }
     });
 });
